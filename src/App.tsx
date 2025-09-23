4f7aa3044c8b052a55827a32e29386af7752022a
protoappxyz <me@shubhanshu.dev>
2025-09-23 15:10:13 -0400

    Setup basic app and css

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css'
+
+export function App() {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Sticky Notes App</h1>
+            <p className={styles.description}>
+                Lets takes notes
+            </p>
+        </div>
+    )
+}
